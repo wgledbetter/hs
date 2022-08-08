ff0cc029fd529719d14929a23bc458958b4d9ea7
William G Ledbetter <williamgledbetter@gmail.com>
2022-08-07 19:10:44 -0600

    unworking BTH SQL example (commented out)

--- src/Scratch/BTH1.hs
+++ src/Scratch/BTH1.hs
@@ -23,3 +23,86 @@ power5' = power 5
 tPower :: (Quote m) => Int -> Code m (Int -> Int)
 tPower 0 = [||\k -> 1||]
 tPower n = [||\k -> k * $$(tPower (n - 1)) k||]
+
+-- -- SQL Example -----------------------------------------------------------------
+
+-- -- Without Templates
+
+-- type Table = [String -> [Int]]
+
+-- type Schema = [String]
+
+-- data Record = Record
+--   { schema :: [ByteString],
+--     fields :: [ByteString]
+--   }
+
+-- type Predicate = Record -> Bool
+
+-- data Operator
+--   = Scan Table
+--   | Project Schema Schema Operator
+--   | Filter Predicate Operator
+--   | Join Operator Operator
+
+-- execOp :: Operator -> IO [Record]
+-- execOp (Project newSchema parentSchema o) = do
+--   rs <- execOp o
+--   return (map (restrict newSchema parentSchema) rs)
+
+-- restrict :: Schema -> Schema -> Record -> Record
+-- restrict = _
+
+-- -- With Templates
+
+-- data Record' = Record'
+--   { schema' :: [ByteString],
+--     fields' :: [Code (* -> *) ByteString]
+--   }
+
+-- data Operator'
+--   = Scan' Table
+--   | Project' Schema Schema Operator'
+--   | Filter' Predicate Operator'
+--   | Join' Operator' Operator'
+
+-- execOp' :: (Monoid m, Quote q) => Operator' -> (Record' -> Code q (IO m)) -> Code q (IO m)
+-- execOp' op yld =
+--   case op of
+--     Scan' file sch -> processCSV sch file yld
+--     Filter' predicate parent ->
+--       execOp'
+--         parent
+--         ( \rcrd ->
+--             [||whenM $$(evalPred predicate rcrd) $$(yld rcrd)||]
+--         )
+--     Project' newSchema parentSchema parent ->
+--       execOp'
+--         parent
+--         ( \rcrd -> yld (restrict' rcrd newSchema parentSchema)
+--         )
+--     Join' left right -> fake
+
+-- processCSV' :: (forall m. Monoid m, Quote q) => Schema -> FilePath -> (Record' -> Code q (IO m)) -> Code q (IO m)
+-- processCSV' ss f yld =
+--   [||
+--   do
+--     bs <- newScanner f
+--     $$(rows ss) bs
+--   ||]
+--   where
+--     rows :: Schema -> Code q (Scanner -> IO m)
+--     rows sch = do
+--       while
+--         [||hasNext||]
+--         [||
+--         \r rs -> do
+--           $$( let (hs, ts) = nextLine [||rs||]
+--                in [||
+--                   $$(yld (Record' (parseRow sch hs) sch))
+--                     <> r $$(ts)
+--                   ||]
+--             )
+--         ||]
+
+-- queryProj :: Operator'
