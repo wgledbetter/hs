047e27bb55213255113f7d36055319aedb47e459
William G Ledbetter <williamgledbetter@gmail.com>
2024-06-27 21:42:04 -0600

    PCP Ch12 server4: comms between client threads

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,18 @@
 module Main where
 
 import qualified PCP.Ch12
+import Text.Printf
 
 -- PCP Dispatch ----------------------------------------------------------------
 
 pcpDispatch :: IO ()
 pcpDispatch = do
-  putStrLn "I've only got Ch12s servers and clients. Give me server# or client#."
+  putStrLn "I've only got Ch12s servers. Give me server#."
   sc <- getLine
   case sc of
     "server0" -> PCP.Ch12.server0
-    "client0" -> PCP.Ch12.client0
+    "server4" -> PCP.Ch12.server4
+    _ -> printf "Ain't got a %s\n" sc
 
 -- Main ------------------------------------------------------------------------
 
@@ -20,3 +22,4 @@ main = do
   book <- getLine
   case book of
     "PCP" -> pcpDispatch
+    _ -> printf "No book named %s\n" book
--- src/PCP/Ch12.hs
+++ src/PCP/Ch12.hs
@@ -1,6 +1,7 @@
 module PCP.Ch12 where
 
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Monad
 import qualified Data.ByteString as BS
 import Data.Text (pack, unpack)
@@ -15,33 +16,13 @@ import Text.Printf
 port :: Int
 port = 44444
 
--- Server 0 --------------------------------------------------------------------
+-- Utils -----------------------------------------------------------------------
 
--- This example uses Network < 3, but Network has been >= 3 since 2018...
--- Prepare for a lot of refactoring.
+sendStr :: Socket -> String -> IO ()
+sendStr sk = sendAll sk <$> (encodeUtf8 . pack)
 
-talk :: Socket -> IO ()
-talk s = do
-  -- Get ByteString from socket
-  msg <- recv s 1024
-  unless (BS.null msg) $ do
-    -- Decode bytes to characters
-    let msgStr = unpack $ decodeUtf8 msg
-    -- get characters before newline
-    let line = head $ lines msgStr
-    if line == "end"
-      then -- The absence of recursion in this case causes termination
-        sendStr "ta ta for now\n"
-      else do
-        sendStr (show (2 * (read line :: Integer)) ++ "\n")
-        -- Recurse to recieve more
-        talk s
-  where
-    -- Convert String to Text to ByteString and send over the socket
-    sendStr = sendAll s <$> (encodeUtf8 . pack)
-
-server0 :: IO ()
-server0 = withSocketsDo $ do
+initSocket :: IO Socket
+initSocket = do
   -- Create hints for address search
   let hints = defaultHints {addrFlags = [AI_PASSIVE], addrSocketType = Stream}
   -- Search for address
@@ -53,15 +34,81 @@ server0 = withSocketsDo $ do
   -- Start listening
   listen sock 1024
   printf "Listening on %s\n" (show addr)
+  -- give it back
+  return sock
+
+-- Server 0 --------------------------------------------------------------------
+
+-- This example uses Network < 3, but Network has been >= 3 since 2018...
+-- Prepare for a lot of refactoring.
+
+-- Single-client service
+talk :: Socket -> IO ()
+talk s = do
+  -- Get ByteString from socket
+  msg <- recv s 1024
+  unless (BS.null msg) $ do
+    -- Decode bytes to characters
+    let msgStr = unpack $ decodeUtf8 msg
+    -- get characters before newline
+    let line = head $ lines msgStr
+    if line == "end"
+      then -- The absence of recursion in this case causes termination
+        sendStr s "ta ta for now\n"
+      else do
+        sendStr s (show (2 * (read line :: Integer)) ++ "\n")
+        -- Recurse to recieve more
+        talk s
+
+server0 :: IO ()
+server0 = withSocketsDo $ do
+  sock <- initSocket
 
   -- Handle connections
   forever $ do
     (conn, addr) <- accept sock
     printf "Accepted connection from %s\n" (show addr)
+    -- Create dedicated thread
     forkFinally (talk conn) (\_ -> close conn)
 
--- Client 0 --------------------------------------------------------------------
+-- Server 3: Broadcast Chan ----------------------------------------------------
 
-client0 :: IO ()
-client0 = withSocketsDo $ do
-  putStrLn "gimme a second"
+-- I _sorta_ want to implement this myself, but not really.
+-- Especially because option 4 uses TVars instead of MVars, which Marlow himself says are simpler.
+-- I already sorta figured that was true, which is why my argus prototypes use TVars
+
+-- Server 4: STM and TVars -----------------------------------------------------
+
+talk4 :: Socket -> TVar Int -> IO ()
+talk4 s f = do
+  msg <- recv s 1024
+  unless (BS.null msg) $ do
+    let msgStr = unpack $ decodeUtf8 msg
+    let line = head $ lines msgStr
+    if line == "end"
+      then sendStr s "ta ta for now\n"
+      else
+        if head line == '*'
+          then do
+            let newFac = read $ tail line :: Int
+            atomically $ writeTVar f newFac
+            sendStr s ("Now multiplying by " ++ show newFac ++ "\n")
+            talk4 s f
+          else do
+            let v = read line :: Int
+            ff <- readTVarIO f
+            sendStr s (show (ff * v) ++ "\n")
+            talk4 s f
+
+server4 :: IO ()
+server4 = withSocketsDo $ do
+  sock <- initSocket
+  fac <- newTVarIO (2 :: Int)
+
+  forever $ do
+    (conn, addr) <- accept sock
+    printf "Accepted connection from %s\n" (show addr)
+    forkFinally (talk4 conn fac) (\_ -> close conn)
+
+-- Woooo! Did it by my's self.
+-- Buuut... It does seem to take a while to release the port after I Ctrl+C it...
